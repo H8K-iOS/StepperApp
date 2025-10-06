186a4f82407145c10de422d62c2ff9819edd2293
H8K-iOS <oleksandr.alimov18@gmail.com>
2025-10-06 21:20:49 +0200

    1

---
+++ Stepper/Utilities/Helpers.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public func isUnder10k(_ count: Int) -> Bool {
+    count < 10000
+}
--- Stepper/Views/ContentView.swift
+++ Stepper/Views/ContentView.swift
@@ -2,14 +2,26 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var healthStore = HealtStore()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello")
+            List(healthStore.steps) { step in
+                HStack {
+                    Circle()
+                        .frame(width: 10, height: 10)
+                        .foregroundStyle(isUnder10k(step.count) ? .red : .green)
+                    Text("\(step.count)")
+                    Spacer()
+                    Text(step.date.formatted(date: .abbreviated, time: .omitted))
+                }
+            }
         }.task {
             await healthStore.requestAuth()
+            do {
+                try await healthStore.getSteps()
+            } catch {
+                print(error)
+            }
         }
         .padding()
     }
